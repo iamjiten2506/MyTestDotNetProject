f565e465e57fcd1ae040477a00d20c3ecb4b01ec
iamjiten2506 <iamjiten@amazon.com>
2025-11-01 11:02:05 +0000

    Create PerformanceTests.cs

---
+++ tests/MyApp.Tests/PerformanceTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyApp;
+using System.Diagnostics;
+
+namespace MyApp.Tests;
+
+[TestClass]
+[TestCategory("Performance")]
+public class PerformanceTests
+{
+    private Calculator _calculator = new();
+
+    [TestMethod]
+    [TestCategory("Performance")]
+    public void Calculator_PerformanceTest_CompletesInTime()
+    {
+        TestContext.WriteLine("Starting performance test");
+        var stopwatch = Stopwatch.StartNew();
+        
+        for (int i = 0; i < 1000000; i++)
+        {
+            _calculator.Add(i, i + 1);
+        }
+        
+        stopwatch.Stop();
+        TestContext.WriteLine($"Performance test completed in {stopwatch.ElapsedMilliseconds}ms");
+        Assert.IsTrue(stopwatch.ElapsedMilliseconds < 5000, "Performance test should complete within 5 seconds");
+    }
+
+    public TestContext TestContext { get; set; } = null!;
+}
