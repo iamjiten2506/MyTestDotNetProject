11a8a380d15a9d7f9139890875c7b4cb30134f6e
iamjiten2506 <iamjiten@amazon.com>
2025-11-01 11:01:06 +0000

    Create IntegrationTests.cs

---
+++ tests/MyApp.Tests/IntegrationTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyApp;
+
+namespace MyApp.Tests;
+
+[TestClass]
+[TestCategory("Integration")]
+public class IntegrationTests
+{
+    private Calculator _calculator = new();
+
+    [TestMethod]
+    [TestCategory("Integration")]
+    public void Calculator_MultipleOperations_WorksTogether()
+    {
+        TestContext.WriteLine("Testing multiple calculator operations");
+        var result1 = _calculator.Add(10, 5);
+        var result2 = _calculator.Multiply(result1, 2);
+        Assert.AreEqual(30, result2);
+        TestContext.WriteLine($"Final result: {result2}");
+    }
+
+    public TestContext TestContext { get; set; } = null!;
+}
